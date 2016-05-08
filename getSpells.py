2844212619cfbb528946fa9e3cd55781c373f627
Yuri Vaz <yurigava@gmail.com>
2016-05-08 01:58:21 -0300

    Creation of fork and some changes:
    - I'll work on my fork so we can discuss changes I make.
    - Changed source site.
    - Created atributos, getting only the first five strongs.
    - Imported lybrary and implemented method to save objects.

--- getSpells.py
+++ getSpells.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.5
 
+import pickle
 import lxml.html as lh
 from urllib.request import urlopen
 from urllib.parse import urljoin
@@ -20,18 +21,18 @@ class playerClass:
     def createClassSpellList(self):
         self.getTree()
         self.classSpellList = self.tree.xpath('//div[@class="wrapper"]/ul/li/a/text()')
-        print(self.classSpellList)
     def getTree(self):
         self.tree = lh.parse(urlopen(self.url)) if self.tree == None else self.tree
         return self.tree
 
-
 class spell:
+    atributos = []
     def __init__(self, spellName, spellUrl):
         self.name = spellName
         self.url = spellUrl
         self.level = ""
         self.tree = None
+        self.getAtributos()
     def getName(self):
         return self.name
     def getUrl(self):
@@ -43,6 +44,13 @@ class spell:
     def getTree(self):
         self.tree = lh.parse(urlopen(self.url)) if self.tree == None else self.tree
         return self.tree
+    def getAtributos(self):
+        self.getTree()
+        atr = self.tree.xpath('//div[@class="post"]/article/p[position() <= 5]/strong/text()')
+        for item in atr:
+            if item not in spell.atributos:
+                spell.atributos.append(item)
+        print("\n\n" + "\n".join(spell.atributos))
 
 class main:
     pageUrl = ""
@@ -85,17 +93,21 @@ class main:
         main.getTree()
         cl = main.tree.xpath('//div[@class="home"]/ul/li/a/text()')
         ur = [urljoin(main.pageUrl,"".join(main.tree.xpath('//a[text()="%s"]/@href' % c))) for c in cl]
-        for count, curClass in enumerate(cl):
-            main.fullSpellList.append(spell(curClass, ur[count]))
+        for count, curSpell in enumerate(cl):
+            main.fullSpellList.append(spell(curSpell, ur[count]))
+        main.sortFullSpellList()
     @staticmethod
     def sortFullSpellList():
         main.fullSpellList.sort(key=lambda spell: spell.name)
+    @staticmethod
+    def saveObject(obj, filename):
+        with open(filename, 'wb') as output:
+            pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
-main("http://ephe.github.io/grimoire")
+main("http://yurigava.github.io/mygrimoire")
 main.createClassList()
 main.createLevelList()
 main.createSpellList()
-main.sortFullSpellList()
 #print(main.spellLevels)
 #print("Class list:")
 #print("\n".join(["'" + element.getName() + "' at --> " + element.getUrl() for element in main.classesList]))
